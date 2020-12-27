f98233957d230658756047b1dd4dd32a5008a406
Alexey Boltykhov <aboltykhov@mail.ru>
2020-12-27 14:25:50 +0000

    Ежедневное обновление

--- web_server_bak.sh
+++ web_server_bak.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
+#Переходим в каталог
+cd /home/adminroot/dz_web_server
 
+#Переключаемся на нашу ветку
+git checkout main
+
+#Добавлем весь каталог в отслеживание
+git add -A
+
+git commit -m "Ежедневное обновление"
+
+#Добавляем репозиторий на github.com
+git remote add origin git@github.com:aboltykhov/dz_web_server.git
+
+#Отправляем изменения
+git push -u origin main
 
