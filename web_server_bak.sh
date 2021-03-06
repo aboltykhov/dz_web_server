2f6b7c7377a1bec3f0bfdf1c19ead1bc38caadd6
Alexey Boltykhov <aboltykhov@mail.ru>
2021-03-06 13:44:13 +0000

    Ежедневное обновление

--- web_server_bak.sh
+++ web_server_bak.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Переходим в каталог
-cd /home/adminroot/dz_web_server
+cd /tmp/dz_web_server
 
 #Переключаемся на нашу ветку
 git checkout main
@@ -18,4 +18,4 @@ git push -u origin main
 
 #запускать скрипт каждый 3й день в 01:00
 #crontab -e
-#0 1 3 * * /home/adminroot/dz_web_server/web_server_bak.sh`
+#0 1 3 * * /tmp/dz_web_server/web_server_bak.sh`
--- web_server_deploy.sh
+++ web_server_deploy.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 #Переходим в каталог
-cd /home/adminroot/dz_web_server
+cd /tmp/dz_web_server
 
 #Переключаемся на нашу ветку main
 git checkout main
 
 #Скачиваем изменения
-git pull
+git pull origin main && ls -lh
 
+#Создаем папки
+mkdir /var/www/8080 /var/www/8081 /var/www/8082
+
+#Разворачиваем бекап
+#Ключ -a копировать содежимое с атрибутами
+cp -a web.bak/var/www/8080/index.html /var/www/8080
+cp -a web.bak/var/www/8081/index.html /var/www/8081
+cp -a web.bak/var/www/8082/index.html /var/www/8082
+
+cp -a web.bak/etc/httpd/conf.d/8080.conf /etc/httpd/conf.d/
+cp -a web.bak/etc/httpd/conf.d/8081.conf /etc/httpd/conf.d/
+cp -a web.bak/etc/httpd/conf.d/8082.conf /etc/httpd/conf.d/
+cp -a web.bak/etc/httpd/conf/httpd.* /etc/httpd/conf/
+
+#Перечитать конфигурацию и показать статус
+systemctl restart httpd && systemctl status httpd
