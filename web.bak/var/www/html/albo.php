ebd2f070a1217c4a4d0bbfc89d4350a6af54f698
Alexey Boltykhov <aboltykhov@mail.ru>
2021-03-08 10:35:52 +0000

    Ежедневное обновление

---
+++ web.bak/var/www/html/albo.php
@@ -0,0 +1,18 @@
+#Скрипт PHP, который будет подключаться к mysql и запрашивать содержимое
+<?php
+$user = "wpuser";
+$password = "WP1password$";
+$database = "wordpress";
+$table = "list";
+
+try {
+  $db = new PDO("mysql:host=localhost;dbname=$database", $user, $password);
+  echo "<h2>TODO</h2><ol>";
+  foreach($db->query("SELECT content FROM $table") as $row) {
+    echo "<li>" . $row['content'] . "</li>";
+  }
+  echo "</ol>";
+} catch (PDOException $e) {
+    print "Error!: " . $e->getMessage() . "<br/>";
+    die();
+}
